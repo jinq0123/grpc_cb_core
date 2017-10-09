6cb9e45800df97d00e53c9bc4a9655e4a29b8b27
Jin Qing <jinq0123@163.com>
2017-10-09 17:55:28 +0800

    Add client_async_reader.cc.

--- include/grpc_cb_core/client/client_async_reader.h
+++ include/grpc_cb_core/client/client_async_reader.h
@@ -5,35 +5,28 @@
 #define GRPC_CB_CORE_CLIENT_ASYNC_READER_H
 
 #include <cstdint>  // for int64_t
-#include <functional>  // for std::function
 #include <string>
 
 #include <grpc_cb_core/client/channel_sptr.h>  // for ChannelSptr
-#include <grpc_cb_core/client/impl/client_async_read_handler.h>  // for ClientAsyncReadHandler
-#include <grpc_cb_core/client/impl/client_async_reader_impl.h>  // for ClientAsyncReaderImpl
 #include <grpc_cb_core/client/msg_str_cb.h>  // for MsgStrCb
 #include <grpc_cb_core/client/status_cb.h>  // for StatusCb
 #include <grpc_cb_core/common/completion_queue_sptr.h>  // for CompletionQueueSptr
+#include <grpc_cb_core/common/support/config.h>  // for GRPC_FINAL
 
 namespace grpc_cb_core {
+
+class ClientAsyncReaderImpl;
 
 // Copyable. Thread-safe.
 class ClientAsyncReader GRPC_FINAL {
  public:
   ClientAsyncReader(const ChannelSptr& channel, const std::string& method,
                     const std::string& request,
-                    const CompletionQueueSptr& cq_sptr, int64_t timeout_ms)
-      : impl_sptr_(new ClientAsyncReaderImpl(channel, method, request, cq_sptr,
-                                             timeout_ms)) {}
+                    const CompletionQueueSptr& cq_sptr, int64_t timeout_ms);
 
  public:
   void ReadEach(const MsgStrCb& msg_cb,
-      const StatusCb& status_cb = StatusCb()) const {
-    auto handler_sptr = std::make_shared<ClientAsyncReadHandler>(msg_cb);
-    impl_sptr_->SetReadHandler(handler_sptr);
-    impl_sptr_->SetStatusCb(status_cb);
-    impl_sptr_->Start();
-  }
+      const StatusCb& status_cb = StatusCb()) const;
 
  private:
   const std::shared_ptr<ClientAsyncReaderImpl> impl_sptr_;
---
+++ src/client/client_async_reader.cc
@@ -0,0 +1,34 @@
+// Licensed under the Apache License, Version 2.0.
+// Author: Jin Qing (http://blog.csdn.net/jq0123)
+
+#include <grpc_cb_core/client/client_async_reader.h>
+
+#include <cstdint>  // for int64_t
+#include <string>
+
+#include <grpc_cb_core/client/channel_sptr.h>  // for ChannelSptr
+#include <grpc_cb_core/client/impl/client_async_read_handler.h>  // for ClientAsyncReadHandler
+#include <grpc_cb_core/client/impl/client_async_reader_impl.h>  // for ClientAsyncReaderImpl
+#include <grpc_cb_core/client/msg_str_cb.h>  // for MsgStrCb
+#include <grpc_cb_core/client/status_cb.h>  // for StatusCb
+#include <grpc_cb_core/common/completion_queue_sptr.h>  // for CompletionQueueSptr
+
+namespace grpc_cb_core {
+
+ClientAsyncReader::ClientAsyncReader(const ChannelSptr& channel,
+                                     const std::string& method,
+                                     const std::string& request,
+                                     const CompletionQueueSptr& cq_sptr,
+                                     int64_t timeout_ms)
+    : impl_sptr_(new ClientAsyncReaderImpl(channel, method, request, cq_sptr,
+                                           timeout_ms)) {}
+
+void ClientAsyncReader::ReadEach(
+    const MsgStrCb& msg_cb, const StatusCb& status_cb /*= StatusCb()*/) const {
+  auto handler_sptr = std::make_shared<ClientAsyncReadHandler>(msg_cb);
+  impl_sptr_->SetReadHandler(handler_sptr);
+  impl_sptr_->SetStatusCb(status_cb);
+  impl_sptr_->Start();
+}
+
+}  // namespace grpc_cb_core
