ebe4569589100f8e9bddd2ab4b69f53551b75830
Jin Qing <jinq0123@163.com>
2017-10-09 18:38:15 +0800

    Add client/client_sync_reader.cc.

--- include/grpc_cb_core/client/client_sync_reader.h
+++ include/grpc_cb_core/client/client_sync_reader.h
@@ -1,47 +1,33 @@
 // Licensed under the Apache License, Version 2.0.
 // Author: Jin Qing (http://blog.csdn.net/jq0123)
-
 #ifndef GRPC_CB_CORE_CLIENT_SYNC_READER_H
 #define GRPC_CB_CORE_CLIENT_SYNC_READER_H
 
-#include <cassert>  // for assert()
 #include <cstdint>  // for int64_t
 #include <string>
 
-#include <grpc_cb_core/client/channel.h>  // for MakeSharedCall()
+#include <grpc_cb_core/client/channel_sptr.h>  // for ChannelSptr
 #include <grpc_cb_core/client/impl/client_sync_reader_data.h>  // for ClientSyncReaderDataSptr
-#include <grpc_cb_core/client/impl/client_reader_init_cqtag.h>  // for ClientReaderInitCqTag
-#include <grpc_cb_core/client/impl/client_sync_reader_helper.h>  // for ClientSyncReaderHelper
-#include <grpc_cb_core/common/status.h>  // for Status
+#include <grpc_cb_core/common/support/config.h>                // for GRPC_FINAL
 
 namespace grpc_cb_core {
+
+class Status;
+struct ClientSyncReaderData;
 
 // Copyable. Client sync reader.
 class ClientSyncReader GRPC_FINAL {
  public:
-  inline ClientSyncReader(const ChannelSptr& channel, const std::string& method,
-                          const std::string& request, int64_t timeout_ms);
+  ClientSyncReader(const ChannelSptr& channel, const std::string& method,
+                   const std::string& request, int64_t timeout_ms);
 
  public:
   // Return false if error or end of stream.
-  bool ReadOne(std::string* response) const {
-    assert(response);
-    Data& d = *data_sptr_;
-    return ClientSyncReaderHelper::SyncReadOne(d.call_sptr, d.cq4p_sptr,
-                                               *response, d.status);
-  }
-
-  Status RecvStatus() const {
-    const Data& d = *data_sptr_;
-    if (!d.status.ok()) return d.status;
-    return ClientSyncReaderHelper::SyncRecvStatus(d.call_sptr, d.cq4p_sptr);
-  }
+  bool ReadOne(std::string* response) const;
+  Status RecvStatus() const;
 
   // Set error status to break reading. Such as when parsing message failed.
-  void SetErrorStatus(const Status& error_status) {
-    assert(!error_status.ok());
-    data_sptr_->status = error_status;
-  }
+  void SetErrorStatus(const Status& error_status);
 
  private:
   // Wrap all data in shared struct pointer to make copy quick.
@@ -50,26 +36,5 @@ class ClientSyncReader GRPC_FINAL {
   DataSptr data_sptr_;
 };  // class ClientSyncReader
 
-ClientSyncReader::ClientSyncReader(
-    const ChannelSptr& channel, const std::string& method,
-    const std::string& request, int64_t timeout_ms)
-    : data_sptr_(new Data) {
-  assert(channel);
-  CQueueForPluckSptr cq4p_sptr(new CQueueForPluck);
-  CallSptr call_sptr = channel->MakeSharedCall(method, *cq4p_sptr, timeout_ms);
-  data_sptr_->cq4p_sptr = cq4p_sptr;
-  data_sptr_->call_sptr = call_sptr;
-
-  ClientReaderInitCqTag tag(call_sptr);
-  if (tag.Start(request)) {
-    cq4p_sptr->Pluck(&tag);
-    return;
-  }
-
-  data_sptr_->status.SetInternalError(
-      "Failed to start client sync reader stream.");
-}  // ClientSyncReader()
-
 }  // namespace grpc_cb_core
-
 #endif  // GRPC_CB_CORE_CLIENT_SYNC_READER_H
---
+++ src/client/client_sync_reader.cc
@@ -0,0 +1,59 @@
+// Licensed under the Apache License, Version 2.0.
+// Author: Jin Qing (http://blog.csdn.net/jq0123)
+
+#include <grpc_cb_core/client/client_sync_reader.h>
+
+#include <cassert>  // for assert()
+#include <cstdint>  // for int64_t
+#include <string>
+
+#include <grpc_cb_core/client/channel.h>  // for MakeSharedCall()
+#include <grpc_cb_core/client/impl/client_reader_init_cqtag.h>  // for ClientReaderInitCqTag
+#include <grpc_cb_core/client/impl/client_sync_reader_data.h>  // for Data
+#include <grpc_cb_core/client/impl/client_sync_reader_helper.h>  // for ClientSyncReaderHelper
+#include <grpc_cb_core/common/status.h>                          // for Status
+
+namespace grpc_cb_core {
+
+ClientSyncReader::ClientSyncReader(const ChannelSptr& channel,
+                                   const std::string& method,
+                                   const std::string& request,
+                                   int64_t timeout_ms)
+    : data_sptr_(new Data) {
+  assert(channel);
+  CQueueForPluckSptr cq4p_sptr(new CQueueForPluck);
+  CallSptr call_sptr = channel->MakeSharedCall(method, *cq4p_sptr, timeout_ms);
+  data_sptr_->cq4p_sptr = cq4p_sptr;
+  data_sptr_->call_sptr = call_sptr;
+
+  ClientReaderInitCqTag tag(call_sptr);
+  if (tag.Start(request)) {
+    cq4p_sptr->Pluck(&tag);
+    return;
+  }
+
+  data_sptr_->status.SetInternalError(
+      "Failed to start client sync reader stream.");
+}  // ClientSyncReader()
+
+// Return false if error or end of stream.
+bool ClientSyncReader::ReadOne(std::string* response) const {
+  assert(response);
+  Data& d = *data_sptr_;
+  return ClientSyncReaderHelper::SyncReadOne(d.call_sptr, d.cq4p_sptr,
+                                             *response, d.status);
+}
+
+Status ClientSyncReader::RecvStatus() const {
+  const Data& d = *data_sptr_;
+  if (!d.status.ok()) return d.status;
+  return ClientSyncReaderHelper::SyncRecvStatus(d.call_sptr, d.cq4p_sptr);
+}
+
+// Set error status to break reading. Such as when parsing message failed.
+void ClientSyncReader::SetErrorStatus(const Status& error_status) {
+  assert(!error_status.ok());
+  data_sptr_->status = error_status;
+}
+
+}  // namespace grpc_cb_core
