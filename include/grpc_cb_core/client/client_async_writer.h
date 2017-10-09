3719deaebb9d926d0c3174d77c7b8ca99d9f793a
Jin Qing <jinq0123@163.com>
2017-10-09 18:26:02 +0800

    Add client/client_async_writer.cc.

--- include/grpc_cb_core/client/client_async_writer.h
+++ include/grpc_cb_core/client/client_async_writer.h
@@ -4,46 +4,30 @@
 #ifndef GRPC_CB_CORE_CLIENT_ASYNC_WRITER_H
 #define GRPC_CB_CORE_CLIENT_ASYNC_WRITER_H
 
-#include <cassert>  // for assert()
 #include <cstdint>  // for int64_t
 #include <string>
 
-#include <grpc_cb_core/client/channel_sptr.h>  // for ChannelSptr
-#include <grpc_cb_core/client/close_cb.h>  // for CloseCb
-#include <grpc_cb_core/client/impl/client_async_writer_close_handler.h>  // for ClientAsyncWriterCloseHandler
-#include <grpc_cb_core/client/impl/client_async_writer_impl.h>  // for ClientAsyncWriterImpl
+#include <grpc_cb_core/client/channel_sptr.h>           // for ChannelSptr
+#include <grpc_cb_core/client/close_cb.h>               // for CloseCb
 #include <grpc_cb_core/common/completion_queue_sptr.h>  // for CompletionQueueSptr
-#include <grpc_cb_core/common/support/config.h>  // for GRPC_OVERRIDE
+#include <grpc_cb_core/common/support/config.h>         // for GRPC_FINAL
 
 namespace grpc_cb_core {
 
+class ClientAsyncWriterImpl;
 class Status;
 
 // Copyable. Thread-safe.
 class ClientAsyncWriter GRPC_FINAL {
  public:
-  inline ClientAsyncWriter(const ChannelSptr& channel,
-                           const std::string& method,
-                           const CompletionQueueSptr& cq_sptr,
-                           int64_t timeout_ms)
-      // Todo: same as ClientReader?
-      : impl_sptr_(new ClientAsyncWriterImpl(channel, method, cq_sptr,
-                                             timeout_ms)) {
-    assert(channel);
-    assert(cq_sptr);
-  }
+  ClientAsyncWriter(const ChannelSptr& channel, const std::string& method,
+                    const CompletionQueueSptr& cq_sptr, int64_t timeout_ms);
 
   // Todo: Get queue size()
   // Todo: SyncGetInitMd();
 
-  bool Write(const std::string& request) const {
-    return impl_sptr_->Write(request);
-  }
-
-  void Close(const CloseCb& close_cb = CloseCb()) {
-    auto handler = std::make_shared<ClientAsyncWriterCloseHandler>(close_cb);
-    impl_sptr_->Close(handler);
-  }  // Close()
+  bool Write(const std::string& request) const;
+  void Close(const CloseCb& close_cb = CloseCb());
 
   // Todo: Use a default CloseHandler if no Close()?
 
---
+++ src/client/client_async_writer.cc
@@ -0,0 +1,41 @@
+// Licensed under the Apache License, Version 2.0.
+// Author: Jin Qing (http://blog.csdn.net/jq0123)
+
+#include <grpc_cb_core/client/client_async_writer.h>
+
+#include <cassert>  // for assert()
+#include <cstdint>  // for int64_t
+#include <string>
+
+#include <grpc_cb_core/client/channel_sptr.h>  // for ChannelSptr
+#include <grpc_cb_core/client/close_cb.h>      // for CloseCb
+#include <grpc_cb_core/client/impl/client_async_writer_close_handler.h>  // for ClientAsyncWriterCloseHandler
+#include <grpc_cb_core/client/impl/client_async_writer_impl.h>  // for ClientAsyncWriterImpl
+#include <grpc_cb_core/common/completion_queue_sptr.h>  // for CompletionQueueSptr
+
+namespace grpc_cb_core {
+
+ClientAsyncWriter::ClientAsyncWriter(const ChannelSptr& channel,
+                                     const std::string& method,
+                                     const CompletionQueueSptr& cq_sptr,
+                                     int64_t timeout_ms)
+    // Todo: same as ClientReader?
+    : impl_sptr_(
+          new ClientAsyncWriterImpl(channel, method, cq_sptr, timeout_ms)) {
+  assert(channel);
+  assert(cq_sptr);
+}
+
+// Todo: Get queue size()
+// Todo: SyncGetInitMd();
+
+bool ClientAsyncWriter::Write(const std::string& request) const {
+  return impl_sptr_->Write(request);
+}
+
+void ClientAsyncWriter::Close(const CloseCb& close_cb/* = CloseCb()*/) {
+  auto handler = std::make_shared<ClientAsyncWriterCloseHandler>(close_cb);
+  impl_sptr_->Close(handler);
+}  // Close()
+
+}  // namespace grpc_cb_core
