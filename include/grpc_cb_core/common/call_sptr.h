f7fe0ae6a0c5e09d3d90e1008773c00f13dc10fb
Jin Qing <jinq0123@163.com>
2017-10-09 12:06:01 +0800

    Change macro.

--- include/grpc_cb_core/common/call_sptr.h
+++ include/grpc_cb_core/common/call_sptr.h
@@ -1,8 +1,8 @@
 // Licensed under the Apache License, Version 2.0.
 // Author: Jin Qing (http://blog.csdn.net/jq0123)
 
-#ifndef GRPC_CB_CORE_IMPL_CALL_SPTR_H
-#define GRPC_CB_CORE_IMPL_CALL_SPTR_H
+#ifndef GRPC_CB_CORE_COMMON_CALL_SPTR_H
+#define GRPC_CB_CORE_COMMON_CALL_SPTR_H
 
 #include <memory>
 
@@ -11,4 +11,4 @@ class Call;
 using CallSptr = std::shared_ptr<Call>;
 }  // namespace grpc_cb_core
 
-#endif  // GRPC_CB_CORE_IMPL_CALL_SPTR_H
+#endif  // GRPC_CB_CORE_COMMON_CALL_SPTR_H
