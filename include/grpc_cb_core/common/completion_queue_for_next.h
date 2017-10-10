27f8f5407a40ca1eaf4cbbb3f225f2b8cff52171
Jin Qing <jinq0123@163.com>
2017-10-10 11:48:52 +0800

    Add common/completion_queue_for_next.h.

---
+++ include/grpc_cb_core/common/completion_queue_for_next.h
@@ -0,0 +1,46 @@
+// Complete queue for next.
+#ifndef GRPC_CB_CORE_COMMON_COMPLETION_QUEUE_FOR_NEXT_H
+#define GRPC_CB_CORE_COMMON_COMPLETION_QUEUE_FOR_NEXT_H
+
+#include <grpc_cb_core/common/completion_queue.h>  // for CompletionQueue
+
+namespace grpc_cb_core {
+
+/// A thin wrapper around grpc_completion_queue for next.
+/// Thread-safe.
+class CompletionQueueForNext : public CompletionQueue {
+ public:
+  /// Default constructor. Implicitly creates a \a grpc_completion_queue
+  /// instance.
+  CompletionQueueForNext();
+
+  /// Wrap \a take, taking ownership of the instance.
+  ///
+  /// \param take The completion queue instance to wrap. Ownership is taken.
+  explicit CompletionQueueForNext(grpc_completion_queue* take);
+
+  /// Destructor. Destroys the owned wrapped completion queue instance.
+  virtual ~CompletionQueueForNext() GRPC_OVERRIDE;
+
+ public:
+  /// Wraps \a grpc_completion_queue_next.
+  /// \param deadline[in] How long to block in wait for an event.
+  template <typename T>
+  grpc_event Next(const T& deadline) {
+    TimePoint<T> deadline_tp(deadline);
+    return NextInternal(deadline_tp.raw_time());
+  }
+
+  grpc_event Next() {
+    return NextInternal(gpr_inf_future(GPR_CLOCK_REALTIME));
+  }
+  grpc_event TryNext() {
+    return NextInternal(gpr_time_0(GPR_CLOCK_REALTIME));
+  }
+
+ private:
+  grpc_event NextInternal(gpr_timespec deadline);
+};
+
+}  // namespace grpc_cb_core
+#endif  // GRPC_CB_CORE_COMMON_COMPLETION_QUEUE_FOR_NEXT_H
