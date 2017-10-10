56577f664d02b4c467b5c0a9daa2e7ff38c724a7
Jin Qing <jinq0123@163.com>
2017-10-10 10:18:39 +0800

    Move grpc_library.h to support dir.

--- include/grpc_cb_core/common/completion_queue.h
+++ include/grpc_cb_core/common/completion_queue.h
@@ -40,7 +40,7 @@
 #include <memory>  // for unique_ptr<>
 
 #include <grpc_cb_core/common/completion_queue_sptr.h>  // for CompletionQueueSptr
-#include <grpc_cb_core/common/impl/grpc_library.h>           // for GrpcLibrary
+#include <grpc_cb_core/common/support/grpc_library.h>           // for GrpcLibrary
 #include <grpc_cb_core/common/support/config.h>  // for GRPC_OVERRIDE
 
 struct grpc_completion_queue;
