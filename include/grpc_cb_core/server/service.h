1100bfc46436adc1be82599be42b678425401eca
Jin Qing <jinq0123@163.com>
2017-10-11 12:09:58 +0800

    Format.

--- include/grpc_cb_core/server/service.h
+++ include/grpc_cb_core/server/service.h
@@ -1,12 +1,11 @@
 // Licensed under the Apache License, Version 2.0.
 // Author: Jin Qing (http://blog.csdn.net/jq0123)
-
-#ifndef GRPC_CB_CORE_SERVICE_H
-#define GRPC_CB_CORE_SERVICE_H
+#ifndef GRPC_CB_CORE_SERVER_SERVICE_H
+#define GRPC_CB_CORE_SERVER_SERVICE_H
 
 #include <string>
 
-#include <grpc_cb_core/common/call_sptr.h>            // for CallSptr
+#include <grpc_cb_core/common/call_sptr.h>                 // for CallSptr
 #include <grpc_cb_core/common/support/grpc_cb_core_api.h>  // for GRPC_CB_CORE_API
 
 struct grpc_byte_buffer;
@@ -24,8 +23,7 @@ class GRPC_CB_CORE_API Service {
   // TODO: need request_context. Need client address in Ctr?
   virtual void CallMethod(size_t method_index, grpc_byte_buffer* request,
                           const CallSptr& call_sptr) = 0;
-};
+};  // class Service
 
 }  // namespace grpc_cb_core
-
-#endif  // GRPC_CB_CORE_SERVICE_H
+#endif  // GRPC_CB_CORE_SERVER_SERVICE_H
